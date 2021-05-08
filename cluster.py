657acba0f93defce3faa8ec3fadead1d1f25233e
Jibran Khalil <57240333+JibKh@users.noreply.github.com>
2021-05-08 21:06:41 +0500

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -3,11 +3,7 @@ import shutil
 import cv2
 import os
 import math
-import matplotlib.pyplot as plt
-from kneed import KneeLocator
-from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 
 
